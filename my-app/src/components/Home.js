15e58e6ec6de31d0ea9be678c136768db57c0d0d
kevinneyer <kevinneyer@gmail.com>
2020-09-21 21:34:00 -0400

    home page css

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,16 +1,33 @@
 import React from 'react'
-import {albumCover} from '../images/albumCover.jpg'
-import {Icon} from 'semantic-ui-react'
+import albumCover from '../assets/albumCover.jpg'
+import organKeys from '../assets/organKeys.JPG'
+import { Icon, Grid, Image, Label, Segment} from 'semantic-ui-react'
 
 const Home = () => {
     return(
         <div>
             <h1>Home</h1>
-            <img src={albumCover} alt='album cover' />
-                <Icon name='instagram' size='big'/>
-                <Icon name='facebook' size='big' color='blue' />
-                <Icon name='spotify' size='big' color='green' />
-                <Icon name='apple' size='big' />
+            <Grid>
+            <Grid.Column>
+            <Segment className='ep' raised>
+                <Label color='red'>
+                Available Now!
+                </Label>
+                <Image src={albumCover} alt='album cover' style={{width: 500}} />
+
+            </Segment>
+            </Grid.Column>
+            </Grid>
+            <div className='social-div'>
+                <Segment>
+                <Icon className='socials' name='instagram' size='big'/>
+                <Icon className='socials' name='facebook' size='big' color='blue' />
+                <Icon className='socials' name='spotify' size='big' color='green' />
+                <Icon className='socials' name='apple' size='big' />
+                </Segment>
+            </div>
+            
+
         </div>
     )
 }
