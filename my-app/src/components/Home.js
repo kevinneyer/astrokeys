5d8e6cc639764b69472c02857ae9e9dda9c2d187
kevinneyer <kevinneyer@gmail.com>
2020-09-22 20:14:04 -0400

    updated socials

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -19,10 +19,10 @@ const Home = () => {
            
             <div className='social-div'>
                 <Segment>
-                <Icon className='socials' name='instagram' size='big'/>
-                <Icon className='socials' name='facebook' size='big' color='blue' />
-                <Icon className='socials' name='spotify' size='big' color='green' />
-                <Icon className='socials' name='apple' size='big' />
+                <a href='https://www.instagram.com/theastrokeys/' target='_blank'><Icon className='socials' name='instagram' size='big' color='black'/></a>
+                <a href='https://www.facebook.com/pg/TheAstroKeys/about/?ref=page_internal' target='_blank'><Icon className='socials' name='facebook' size='big' color='blue' /></a>
+                <a href='https://open.spotify.com/album/0OpEKaIDNyWPhwQmjlQlvs?si=d3vvmiO7S5OwkqY6S5njfA' target='_blank'><Icon className='socials' name='spotify' size='big' color='green' /></a>
+                <a href='https://music.apple.com/us/album/astro-keys-ep/1523101481' target='_blank'><Icon className='socials' name='apple' size='big' color='black'/></a>
                 </Segment>
             </div>
             
