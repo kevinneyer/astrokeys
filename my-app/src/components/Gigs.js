0086b163a41e934d7fc6197dd656048a2d6df33b
kevinneyer <kevinneyer@gmail.com>
2020-09-30 22:59:49 -0400

    final Wednesday night

--- my-app/src/components/Gigs.js
+++ my-app/src/components/Gigs.js
@@ -7,7 +7,7 @@ const Gigs  = () => {
     const [gigs, setGigs] = useState([])
 
     useEffect(() =>{
-       setGigs([{name: 'Guaginos'}, {name: 'Hooray'}, {name: 'We Got Gigs'}]) 
+       setGigs([{name: 'Guaginos', time: '7:00 - 8:00pm', date: '10/10/2020'}, {name: 'Hooray', time: '7:00 - 8:00pm', date: '10/10/2020'}, {name: 'We Got Gigs', time: '7:00 - 8:00pm', date: '10/10/2020'}]) 
     })
 
     return(
@@ -16,15 +16,16 @@ const Gigs  = () => {
 
             {gigs.length > 0 ? gigs.map( gig => 
             <div className='gigs'>
-            <Segment>
+            <Segment raised>
             <Item>
                 <Item.Content>
                     <Item.Header>{gig.name}</Item.Header>
-                    <Item.Meta>Description</Item.Meta>
-                    <Item.Description>
+                    <Item.Meta>Date: {gig.date}</Item.Meta>
+                    <Item.Meta>Time: {gig.time}</Item.Meta>
+                    {/* <Item.Description>
                         Date and Time or Something
                     </Item.Description>
-                    <Item.Extra>Place Deatils</Item.Extra>
+                    <Item.Extra>Place Deatils</Item.Extra> */}
                 </Item.Content>
             </Item>
             </Segment>
